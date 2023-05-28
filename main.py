185f3cf184bf06c4fff103ebc476a51cc90d4901
MateiTiplea <m_tiplea@yahoo.com>
2023-05-28 14:03:49 +0300

    + cors_config

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from model import prediction
 from flask import *
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.get('/api/v1/prediction/<int:credits>')
+@cross_origin('http://localhost:3000')
 def predict(credits):
     if credits < 4 or credits > 6:
         return jsonify({'error': 'Invalid credits'}), 400
