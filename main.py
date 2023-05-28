e436b5475e9b5d441ccf2479c48ef239d1936167
MateiTiplea <m_tiplea@yahoo.com>
2023-05-28 11:07:03 +0300

    + api_banal

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from model import prediction
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.get('/api/v1/prediction/<int:credits>')
+def predict(credits):
+    if credits < 4 or credits > 6:
+        return jsonify({'error': 'Invalid credits'}), 400
+    else:
+        return jsonify({'prediction': prediction(credits)}), 200
+
 
 if __name__ == '__main__':
-    print(prediction(3))
-
-
+    app.run(host='localhost', port=5000)
--- model.py
+++ model.py
@@ -20,16 +20,10 @@ def model_build():
     df = read_dataset()
     hours = np.array(df['hours'].values)
     credits = np.array(df['credits'].values).reshape((-1, 1))
-    # print(hours)
-    # print(credits)
     model = linear_model.LinearRegression()
     model = model.fit(credits, hours)
     intercept = model.intercept_
     slope = model.coef_
-
-    # print(f"intercept: {intercept}")
-    # print(f"slope: {slope} ")
-    # print(f"score: {model.score(credits, hours)}")
     return [intercept, slope]
 
 
